b8bdaee31e0757e0e266b654b552b6ded68a8bfd
geo1590 <george_barr@yahoo.com>
2025-08-23 13:00:42 -0700

    Update server.py

--- app/server.py
+++ app/server.py
@@ -2,6 +2,20 @@ from fastapi import FastAPI
 import joblib
 import numpy as np
 
+from pyngrok import ngrok
+import os
+
+# Open an ngrok tunnel to port web_port.
+web_port = int(os.environ.get("WEB_PORT"))
+print(f'web_port: {web_port}')
+# authtoken = "31hJVxly22Ll452wEszMNTTfssf_y31L2BXNBpaPHrYAbGcp"
+authtoken = "31hYhuo5WYgo57T6dH7N716k1EE_2BJYnoWToy9mA9PB3MxcP"
+    # Sign up for a free account here: https://ngrok.com/signup
+    # Create a AuthToken and assign the python variable authtoken above to this value.
+ngrok.set_auth_token(authtoken)
+public_url = ngrok.connect(web_port)
+print("Public URL:", public_url)
+
 model = joblib.load('app/model.joblib')
 
 class_names = np.array(['setosa', 'versicolor', 'virginica'])
@@ -10,7 +24,8 @@ app = FastAPI()
 
 @app.get('/')
 def read_root():
-    return {'message': 'Iris model API'}
+    # return {'message': f'Iris model API, public_url: {public_url}'}
+    return {'message': f'Iris model API'}
 
 @app.post('/predict')
 def predict(data: dict):
@@ -28,3 +43,5 @@ def predict(data: dict):
     prediction = model.predict(features)
     class_name = class_names[prediction][0]
     return {'predicted_class': class_name}
+
+
